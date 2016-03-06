74bfb805ca2a6b84cc4ed0030efe354e6ed8cbee
murphy <colmurp@gmail.com>
2016-03-06 20:16:39 +0000

    popup arabic

--- app/src/main/java/com/example/murphy/keyb/Keyb.java
+++ app/src/main/java/com/example/murphy/keyb/Keyb.java
@@ -11,7 +11,7 @@ import android.view.MotionEvent;
 import android.view.View;
 
 /**
- * An example full-screen activity that shows and hides the system UI (i.e.
+ * full-screen activity that shows and hides the system UI (i.e.
  * status bar and navigation/system bar) with user interaction.
  */
 public class Keyb extends AppCompatActivity {
